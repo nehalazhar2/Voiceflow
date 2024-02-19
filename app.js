b368385f98a01e81ecbc8730ad2ab4c2b6bd803d
Nehal <nehalazhar2@gmail.com>
2024-02-19 21:13:07 +0500

    final build

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const OpenAI = require('openai');
+const fs = require('fs');
+const axios = require('axios');
+const app = express();
+const port = 3000; // Choose your desired port
+const fetch = require('node-fetch');
+   const FormData = require('form-data');
+// Initialize OpenAI
+const openai = new OpenAI();
+
+app.get('/getFile/:fileid', async (req, res) => {
+    try {
+      const fileid = req.params.fileid;
+      let base64Data=""
+      
+      const myHeaders = {
+        "Authorization": "Bearer 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",
+        "Cookie": "__cf_bm=GYtgnC2MdmLpmBkjfPtHpaddbOL.IunVXAwpSPfa3mA-1708356337-1.0-AWYdlAcDf1SGoGgeF0IQ7gm77Jd+9qLYtNTM+L6xRP67ALcN7oSyfFx/SZwidVuE2xg3CcfFOGQMAp69QmCod/I=; _cfuvid=Knsf9PgrNn3dGtd56LPZtrjda2lDxBa4aueoilWTUmc-1708356337426-0.0-604800000"
+      };
+      
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+      };
+      
+      fetch("https://api.openai.com/v1/files/"+fileid+"/content", requestOptions)
+        .then((response) => response.buffer())
+        .then((result) => {
+          // Convert the received buffer to base64
+           base64Data = result.toString('base64');
+
+      
+       
+let data = new FormData();
+data.append('key', 'a33e1e04a4f3febcb4277c2cc118f5e3');
+data.append('image', base64Data);
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api.imgbb.com/1/upload',
+  headers: { 
+    ...data.getHeaders()
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+ res.send(response.data);
+})
+.catch((error) => {
+  console.log(error);
+});
+        })
+        .catch((error) => console.error(error));
+      
+
+
+
+
+    //   Upload the file to imgBB
+
+      
+    
+  
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
